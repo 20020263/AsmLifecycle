f52f535626da254a52305d30861c2e23b7eecf8f
Mac <mac@MacdeMacBook-Pro.local>
2021-03-20 01:05:40 +0800

    gradle+ASM实现页面耗时打印

---
+++ app/src/main/java/com/hxj/asmdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hxj.asmdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
